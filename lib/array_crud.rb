038214b8e5765d650b66fdd5c2b1e16c0e627f08
Zach <zachlandis91@gmail.com>
2023-04-05 12:42:37 -0600

    commit tree a317871d415ce254c49038f9b84bc094165babaf

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,37 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  arr = [1,2,3,4]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array = []
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array = []
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array.pop()
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array[-1]
 end
